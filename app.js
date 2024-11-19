bb91e72db0d4d23715263da4407dbbdb02e51e7f
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-19 15:28:36 +0100

    homepage express

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  console.log("request recieved");
+  res.send("Homepage");
+});
 app.listen(port, () => {
   console.log("server listening on port", port);
 });
